223ade667192e537881581e51589fac34b60b7e4
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-01-13 15:42:03 +0900

    03. 변수

---
+++ app/src/main/java/fastcampus/part0/kotlin1/Example3.kt
@@ -0,0 +1,15 @@
+package fastcampus.part0.kotlin1
+
+// 03. 변수
+// val = value (값)
+// var = variable (변경가능한)
+
+fun main() {
+    val a : Int = 3
+    var b : Int = 10
+    //a = 20
+    // val : 변경불가  -- var로 바꿀 수 있는 shortcut 기능 제공함
+
+    // 코틀린의 '타입 추론' : 변수 타입 안 적어도 괜찮음
+    val name = "노원희"
+}
