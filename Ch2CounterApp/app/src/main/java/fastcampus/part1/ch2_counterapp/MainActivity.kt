d159c43f41e7a99b44a01f7443230ac4b04aa278
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-01-16 14:25:40 +0900

    APP-1. 숫자 카운트 앱

---
+++ Ch2CounterApp/app/src/main/java/fastcampus/part1/ch2_counterapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package fastcampus.part1.ch2_counterapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val numberTextView = findViewById<TextView>(R.id.numberTextView)
+        val resetButton = findViewById<Button>(R.id.resetButton)
+        val plusButton = findViewById<Button>(R.id.plusButton)
+
+        resetButton.setOnClickListener {
+            Log.d("onClick", "리셋 버튼이 클릭 됐습니다!")
+        }
+
+        plusButton.setOnClickListener {
+            Log.i("onClick", "플러스 버튼이 클릭 됐습니다!")
+        }
+    }
+}
