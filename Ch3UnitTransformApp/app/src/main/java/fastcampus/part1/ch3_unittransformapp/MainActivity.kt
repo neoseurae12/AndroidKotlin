558d398c194910177447397b06366e7da31761d4
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-01-20 12:11:50 +0900

    APP-2. 단위 변환 앱: 단위 변환 기능 구현

---
+++ Ch3UnitTransformApp/app/src/main/java/fastcampus/part1/ch3_unittransformapp/MainActivity.kt
@@ -0,0 +1,55 @@
+package fastcampus.part1.ch3_unittransformapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import fastcampus.part1.ch3_unittransformapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val outputTextView = binding.outputTextView
+        val outputUnitTextView = binding.outputUnitTextView
+        val inputEditText = binding.inputEditText
+        val inputUnitTextView = binding.inputUnitTextView
+        val swapImageButton = binding.swapImageButton
+
+        var inputNumber :Int = 0
+        var cmToM = true
+
+        inputEditText.addTextChangedListener { text ->
+            inputNumber = if(text.isNullOrBlank()) {
+                0
+            } else {
+                text.toString().toInt()
+            }
+
+            if(cmToM) {
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+
+        swapImageButton.setOnClickListener {
+            cmToM = cmToM.not()
+
+            if(cmToM) {
+                inputUnitTextView.text = "cm"
+                outputUnitTextView.text = "m"
+                outputTextView.text = inputNumber.times(0.01).toString()
+            } else {
+                inputUnitTextView.text = "m"
+                outputUnitTextView.text = "cm"
+                outputTextView.text = inputNumber.times(100).toString()
+            }
+        }
+    }
+}
