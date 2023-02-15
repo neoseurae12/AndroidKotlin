18ad879bd45b9cb57a3e9d0b611c920f58a0fd8f
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2023-02-15 10:15:51 +0900

    App-7. 나만의 액자 앱: 갤러리에서 이미지 불러오기

--- Ch8GalleryApp/app/src/main/java/fastcampus/part1/ch8_galleryapp/MainActivity.kt
+++ Ch8GalleryApp/app/src/main/java/fastcampus/part1/ch8_galleryapp/MainActivity.kt
@@ -2,15 +2,21 @@ package fastcampus.part1.ch8_galleryapp
 
 import android.Manifest.permission.READ_MEDIA_IMAGES
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import fastcampus.part1.ch8_galleryapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val imageLoadLauncher = registerForActivityResult(ActivityResultContracts.GetMultipleContents()) { uriList ->
+        updateImages(uriList)
+    }
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -40,19 +46,17 @@ class MainActivity : AppCompatActivity() {
                 requestReadMediaImages()
             }
         }
-
-
     }
 
     private fun loadImage() {
-        Toast.makeText(this, "이미지에 대한 권한이 '허용' 상태입니다", Toast.LENGTH_SHORT).show()
+        imageLoadLauncher.launch("image/*")
     }
 
     private fun showPermissionInfoDialog() {
         AlertDialog.Builder(this).apply {
             setMessage("이미지를 가져오기 위해 외부 저장소 읽기 권한이 필요합니다.")
-            setNegativeButton("취소", null)
-            setPositiveButton("동의") { _, _ ->
+            setNegativeButton("Cancel", null)
+            setPositiveButton("OK") { _, _ ->
                 requestReadMediaImages()
             }
         }.show()
@@ -64,6 +68,27 @@ class MainActivity : AppCompatActivity() {
             arrayOf(READ_MEDIA_IMAGES),
             REQUEST_READ_MEDIA_IMAGES
         )
+    }
+
+    private fun updateImages(uriList: List<Uri>) {
+        Log.d("updateImages", "$uriList")
+    }
+
+    // As soon as you grant app permission, image loading screen appears
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            REQUEST_READ_MEDIA_IMAGES -> {
+                if(grantResults.firstOrNull() == PackageManager.PERMISSION_GRANTED) {
+                    loadImage()
+                }
+            }
+        }
     }
 
     companion object {
